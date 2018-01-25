1cdf3beb5ca78b898ec781f026b52378d21bc051
sergi <sruizcarmona@gmail.com>
2018-01-25 17:42:41 +0100

    symbols test

--- sym/create_html.sh
+++ sym/create_html.sh
@@ -1,4 +1,4 @@
 for f in `awk '{print $1}' FS=":" sym_list.txt`; 
 do 
-	echo "<div class=\"vote-icon\"> <i class=\"fa $f\"> $f </i> </div>"
+	echo "<div class=\"vote-icon\" style=\"color:black\"> <i class=\"fa $f\"> $f </i> </div>"
 done
