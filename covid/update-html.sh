b5af8155dd6284f88f46d9e70726a3ecbc8bf809
Sergio Ruiz Carmona <sruizcarmona@gmail.com>
2020-04-15 13:04:43 +1000

    covidplots update

--- covid/update-html.sh
+++ covid/update-html.sh
@@ -1,4 +1,4 @@
-source /home/sruizcarmona/miniconda3/etc/profile.d/conda.sh
+#source /home/sruizcarmona/miniconda3/etc/profile.d/conda.sh
 conda activate rmarkdown
 
 Rscript -e 'library(rmarkdown); rmarkdown::render("covid_plots.Rmd", encoding = encoding, output_file = "index.html")'
