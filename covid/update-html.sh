f283f1717dd5ce83b2407614590996e739856e6f
Sergio Ruiz Carmona <sruizcarmona@gmail.com>
2020-04-15 14:42:19 +1000

    test git pwd

--- covid/update-html.sh
+++ covid/update-html.sh
@@ -1,6 +1,6 @@
 source /Users/sruizcarmona/miniconda3/etc/profile.d/conda.sh
 conda activate rstudio
 
-#Rscript -e 'library(rmarkdown); rmarkdown::render("covid_plots.Rmd", encoding = encoding, output_file = "index.html")'
+Rscript -e 'library(rmarkdown); rmarkdown::render("covid_plots.Rmd", encoding = encoding, output_file = "index.html")'
 git commit -am 'covidplots update'
 git push origin master
