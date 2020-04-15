84356434a867ab4e4622b0ae0036199fc034483a
Sergio Ruiz Carmona <sruizcarmona@gmail.com>
2020-04-15 12:41:54 +1000

    covidplots update

--- covid/update-html.sh
+++ covid/update-html.sh
@@ -1,4 +1,6 @@
+source /home/sruizcarmona/miniconda3/etc/profile.d/conda.sh
 conda activate rmarkdown
+
 Rscript -e 'library(rmarkdown); rmarkdown::render("covid_plots.Rmd", encoding = encoding, output_file = "index.html")'
 git commit -am 'covidplots update'
 git push origin master
