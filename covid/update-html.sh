34c7d36722d6b23b3bf040cd75ac0e1522740868
Sergio Ruiz Carmona <sruizcarmona@gmail.com>
2020-04-28 09:07:02 +1000

    update fixed

--- covid/update-html.sh
+++ covid/update-html.sh
@@ -6,11 +6,22 @@ conda activate rstudio
 file_date=`GetFileInfo -m covid_data.rda | cut -d' ' -f1 | awk '{print $3$1$2}' FS='/'`
 today=`date  +%Y%m%d`
 
-if [ $file_date -eq $today ];
-then
+# check also that link exists
+today_day=`date +%d`
+today_month=`date +%B | tr '[:upper:]' '[:lower:]'`
+today_url=`echo https://www.dhhs.vic.gov.au/coronavirus-update-victoria-${today_day}-${today_month}-2020`
+today_url2=`echo https://www.dhhs.vic.gov.au/coronavirus-update-victoria-${today_day}-${today_month}`
+
+if [ $file_date -eq $today ]; then
     exit
+elif ! curl --output /dev/null --silent --head --fail "$today_url"; then
+    if ! curl --output /dev/null --silent --head --fail "$today_url2"; then
+        exit
+    fi
 fi
 
+
+# only run following lines if file is from yesterday and the links exist
 Rscript -e 'library(rmarkdown); rmarkdown::render("covid_plots.Rmd", encoding = encoding, output_file = "index.html")'
 git commit -am 'covidplots update'
 git push origin master
