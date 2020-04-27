f0378e456f8893d0e41ba5b47ab905d151655ffc
Sergio Ruiz Carmona <sruizcarmona@gmail.com>
2020-04-27 18:23:27 +1000

    updated better times

--- covid/update-html.sh
+++ covid/update-html.sh
@@ -1,6 +1,16 @@
 source /Users/sruizcarmona/miniconda3/etc/profile.d/conda.sh
 conda activate rstudio
 
+# only run if covid_data.rda is from day before
+# meaning that it has not been yet calculated for the day
+file_date=`GetFileInfo -m covid_data.rda | cut -d' ' -f1 | awk '{print $3$1$2}' FS='/'`
+today=`date  +%Y%m%d`
+
+if [ $file_date -eq $today ];
+then
+    exit
+fi
+
 Rscript -e 'library(rmarkdown); rmarkdown::render("covid_plots.Rmd", encoding = encoding, output_file = "index.html")'
 git commit -am 'covidplots update'
 git push origin master
