347bcc01339feecbb360031c8d2da70e688fb8b5
Sergio Ruiz Carmona <sruizcarmona@gmail.com>
2020-04-29 12:35:50 +1000

    covidplots update

--- covid/update-html.sh
+++ covid/update-html.sh
@@ -9,17 +9,23 @@ today=`date  +%Y%m%d`
 # check also that link exists
 today_day=`date +%d`
 today_month=`date +%B | tr '[:upper:]' '[:lower:]'`
+today_weekday=`date +%A | tr '[:upper:]' '[:lower:]'`
 today_url=`echo https://www.dhhs.vic.gov.au/coronavirus-update-victoria-${today_day}-${today_month}-2020`
 today_url2=`echo https://www.dhhs.vic.gov.au/coronavirus-update-victoria-${today_day}-${today_month}`
+today_url3=`echo https://www.dhhs.vic.gov.au/coronavirus-update-victoria-${today_weekday}-${today_day}-${today_month}`
+today_url4=`echo https://www.dhhs.vic.gov.au/coronavirus-update-victoria-${today_weekday}-${today_day}-${today_month}-2020`
 
 if [ $file_date -eq $today ]; then
     exit
 elif ! curl --output /dev/null --silent --head --fail "$today_url"; then
     if ! curl --output /dev/null --silent --head --fail "$today_url2"; then
-        exit
+        if ! curl --output /dev/null --silent --head --fail "$today_url3"; then
+            if ! curl --output /dev/null --silent --head --fail "$today_url4"; then
+                exit
+            fi
+        fi
     fi
 fi
-
 
 # only run following lines if file is from yesterday and the links exist
 Rscript -e 'library(rmarkdown); rmarkdown::render("covid_plots.Rmd", encoding = encoding, output_file = "index.html")'
