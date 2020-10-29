cf8178040c6cc89acc7aaf91eb16ac0255d72353
Sergio Ruiz <sruizcarmona@gmail.com>
2020-10-29 17:36:55 +1100

    covidplots auto update

--- covid/update-html.sh
+++ covid/update-html.sh
@@ -11,18 +11,20 @@ today_day=`date +%-d`
 today_month=`date +%B | tr '[:upper:]' '[:lower:]'`
 today_weekday=`date +%A | tr '[:upper:]' '[:lower:]'`
 
-url_prefix=""
+url_prefix="/"
+fornotfor=""
 function get_today_url_all () {
     today_day=$1
     url_prefix=$2
+    fornotfor=$3
     today_url_all=(
-    https://www.dhhs.vic.gov.au/${url_prefix}coronavirus-update-victoria-${today_day}-${today_month}-2020
-    https://www.dhhs.vic.gov.au/${url_prefix}coronavirus-update-victoria-${today_day}-${today_month}
-    https://www.dhhs.vic.gov.au/${url_prefix}coronavirus-update-victoria-${today_weekday}-${today_day}-${today_month}
-    https://www.dhhs.vic.gov.au/${url_prefix}coronavirus-update-victoria-${today_weekday}-${today_day}-${today_month}-2020
+    https://www.dhhs.vic.gov.au/${url_prefix}coronavirus-update-${fornotfor}victoria-${today_day}-${today_month}-2020
+    https://www.dhhs.vic.gov.au/${url_prefix}coronavirus-update-${fornotfor}victoria-${today_day}-${today_month}
+    https://www.dhhs.vic.gov.au/${url_prefix}coronavirus-update-${fornotfor}victoria-${today_weekday}-${today_day}-${today_month}
+    https://www.dhhs.vic.gov.au/${url_prefix}coronavirus-update-${fornotfor}victoria-${today_weekday}-${today_day}-${today_month}-2020
     )
 }
-get_today_url_all $today_day  $url_prefix
+get_today_url_all $today_day  $url_prefix $fornotfor
 
 found=0
 if [ $file_date -eq $today ]; then
@@ -34,9 +36,20 @@ else
             break
         fi
     done
+    # try with for before victoria
+    fornotfor="for-"
+    get_today_url_all $today_day $url_prefix $fornotfor
+    for i in "${today_url_all[@]}"; do
+        echo $i
+        if [[ "$found" -eq 0 ]] && curl --output /dev/null --silent --head --fail "$i"; then
+            found=1
+            break
+        fi
+    done
     # try with date with preceded 0
     today_day=`date +%d`
-    get_today_url_all $today_day  $url_prefix
+    fornotfor=""
+    get_today_url_all $today_day $url_prefix $fornotfor
     for i in "${today_url_all[@]}"; do
         if [[ "$found" -eq 0 ]] && curl --output /dev/null --silent --head --fail "$i"; then
             found=1
@@ -46,7 +59,7 @@ else
     #try with different prefix
     today_day=`date +%-d`
     url_prefix="media-release-"
-    get_today_url_all $today_day  $url_prefix
+    get_today_url_all $today_day  $url_prefix $fornotfor
     for i in "${today_url_all[@]}"; do
         if [[ "$found" -eq 0 ]] && curl --output /dev/null --silent --head --fail "$i"; then
             found=1
